598d7fa96a69ecadf58371e46ae5c14992c8782a
Beck Chen <bcfchen@gmail.com>
2015-07-05 00:02:42 -0700

    Add in animation xmls and login activity skeleton code

--- MyCheez/app/src/main/java/com/mycheez/activity/LoginActivity.java
+++ MyCheez/app/src/main/java/com/mycheez/activity/LoginActivity.java
@@ -1,39 +1,291 @@
 package com.mycheez.activity;
 
-import android.support.v7.app.ActionBarActivity;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+//import com.facebook.Request;
+//import com.facebook.Response;
+//import com.facebook.model.GraphUser;
+//import com.parse.FunctionCallback;
+//import com.parse.LogInCallback;
+//import com.parse.ParseCloud;
+//import com.parse.ParseException;
+//import com.parse.ParseFacebookUtils;
+//import com.parse.ParseInstallation;
+//import com.parse.ParseObject;
+//import com.parse.ParseUser;
+//import com.parse.SaveCallback;
 import com.mycheez.R;
+//import com.stealthecheese.application.StealTheCheeseApplication;
+//import com.stealthecheese.enums.UpdateType;
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends Activity {
 
+    private TextView loadingText;
+    private Button loginFBButton;
+    private double timeLeft = 0d;
+
+    private  LinearLayout loadingMsgSection;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginFBButton = (Button) findViewById(R.id.loginButton);
+        loginFBButton.setVisibility(View.GONE);
+
+
+        loadingMsgSection = (LinearLayout) findViewById(R.id.loadingMsgSection);
+        loadingMsgSection.setVisibility(View.GONE);
+
+        loadingText = (TextView) findViewById(R.id.loadingText);
+
+
+        Animation animTranslate  = AnimationUtils.loadAnimation(LoginActivity.this, R.anim.translate);
+        animTranslate.setAnimationListener(new AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation arg0) { }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) { }
+
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                checkNetworkAvailability();
+//                ParseUser currentUser = ParseUser.getCurrentUser();
+//
+//                if ((currentUser != null) && ParseFacebookUtils.isLinked(currentUser)) {
+//                    // user exists
+//                    String stealZoneMsg = getResources().getString(R.string.prep_steal_zone_message);
+//                    showLoadingMsgSection(stealZoneMsg);
+//                    performCreateAndLogin(false);
+//
+//                } else {
+//                    // user does not exists
+//                    loginFBButton.setVisibility(View.VISIBLE);
+//                    Animation animFade  = AnimationUtils.loadAnimation(LoginActivity.this, R.anim.fade);
+//                    loginFBButton.startAnimation(animFade);
+//                    loginFBButton.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            hideLoginButton();
+//                            String signinMsg = getResources().getString(R.string.signing_in_message);
+//                            showLoadingMsgSection(signinMsg);
+//                            loginToFBAndCreateUser();
+//                        }
+//                    });
+//                }
+            }
+        });
+        LinearLayout titleContainer = (LinearLayout) findViewById(R.id.titleContainer);
+        titleContainer.startAnimation(animTranslate);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login, menu);
-        return true;
+    private void showLoadingMsgSection(String message)
+    {
+        loadingMsgSection.setVisibility(View.VISIBLE);
+        Animation animFade  = AnimationUtils.loadAnimation(LoginActivity.this, R.anim.fade);
+        loadingMsgSection.startAnimation(animFade);
+        loadingText.setText(message);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void hideLoadingMsgSection()
+    {
+        loadingMsgSection.setVisibility(View.GONE);
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void checkNetworkAvailability() {
+        ConnectivityManager cm = (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+
+        if (networkInfo == null) {
+            Toast.makeText(this, R.string.no_network_message, Toast.LENGTH_LONG).show();
+            startTheftActivity();
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    private void loginToFBAndCreateUser() {
+        List<String> permissions = Arrays.asList("public_profile", "user_friends");
+//        ParseFacebookUtils.logIn(permissions, this, new LogInCallback() {
+//            @Override
+//            public void done(ParseUser user, ParseException err) {
+//                if(err != null){
+//                    Log.e(StealTheCheeseApplication.LOG_TAG, "Error in creating new user", err);
+//                    hideLoadingMsgSection();
+//                    showLoginButton();
+//                }
+//                if (user == null) {
+//                    Log.i(StealTheCheeseApplication.LOG_TAG, "Uh oh. The user cancelled the Facebook login.");
+//                } else if (user.isNew()) {
+//                    Log.i(StealTheCheeseApplication.LOG_TAG, "User signed up and logged in through Facebook!");
+//                    getFBUserInfo();
+//                } else {
+//                    Log.i(StealTheCheeseApplication.LOG_TAG, "User logged in through Facebook!");
+//                    performCreateAndLogin(false);
+//                }
+//            }
+//        });
+
+    }
+
+    private void showLoginButton()
+    {
+        ViewGroup parentView = (ViewGroup) loginFBButton.getParent();
+        parentView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideLoginButton()
+    {
+        ViewGroup parentView = (ViewGroup) loginFBButton.getParent();
+        parentView.setVisibility(View.GONE);
+    }
+
+    private void performCreateAndLogin(boolean isNewUser){
+        final Map<String,Object> params = new HashMap<String,Object>();
+        params.put("isNewUserFlag", isNewUser);
+
+//        ParseCloud.callFunctionInBackground("onLoginActivity", params, new FunctionCallback<List<ParseUser>>() {
+//
+//            @Override
+//            public void done(List<ParseUser>allUsersData, ParseException ex) {
+//                if (ex == null){
+//                    ParseUser.pinAllInBackground(StealTheCheeseApplication.PIN_TAG, allUsersData, new SaveCallback() {
+//
+//                        @Override
+//                        public void done(ParseException ex) {
+//                            if(ex == null){
+//                                ParseUser curr = ParseUser.getCurrentUser();
+//                                String fbId = (String)curr.get("facebookId");
+//                                ParseInstallation.getCurrentInstallation().put("facebookId", fbId);
+//                                ParseInstallation.getCurrentInstallation().saveInBackground();
+//                                updateCheeseCountData();
+//                            }else {
+//                                Log.e(StealTheCheeseApplication.LOG_TAG, "Error pinning", ex);
+//                            }
+//
+//                        }
+//                    });
+//
+//                }else {
+//                    Log.e(StealTheCheeseApplication.LOG_TAG, "Error fetching data from cloud code: " , ex);
+//                    Toast loginFailedToast = Toast.makeText(getApplicationContext(), R.string.login_failed_message, Toast.LENGTH_LONG);
+//                    loginFailedToast.setGravity(Gravity.CENTER, 0, 0);
+//                    loginFailedToast.show();
+//                }
+//            }
+//        });
+    }
+
+    private void getFBUserInfo() {
+//        loadingText.setText("Getting user profile info...");
+//        final ParseUser loggedInUser = ParseUser.getCurrentUser();
+//        Request request = Request.newMeRequest(ParseFacebookUtils.getSession(), new Request.GraphUserCallback() {
+//            @Override
+//            public void onCompleted(GraphUser user, Response response) {
+//                loggedInUser.put("facebookId", user.getId());
+//                loggedInUser.put("firstName", user.getFirstName());
+//                loggedInUser.put("lastName", user.getLastName());
+//                // Use ProfilePictureView if needed for UI
+//                loggedInUser.put("profilePicUrl", String.format(StealTheCheeseApplication.PROFILE_PIC_URL, user.getId()));
+//                loggedInUser.saveInBackground(new SaveCallback() {
+//
+//                    @Override
+//                    public void done(ParseException parseexception) {
+//                        performCreateAndLogin(true);
+//                    }
+//                });
+//            }
+//        });
+//        request.executeAsync();
+
+    }
+
+
+    private void updateCheeseCountData(){
+        final Map<String,Object> params = new HashMap<String,Object>();
+//        ParseCloud.callFunctionInBackground("getAllCheeseCounts", params, new FunctionCallback<HashMap<String, Object>>() {
+//
+//            @Override
+//            public void done(HashMap<String, Object> wrapper, ParseException ex) {
+//                if(ex == null){
+//                    if(wrapper.containsKey("countDown")){
+//                        timeLeft = (Double)wrapper.get("countDown");
+//                        timeLeft +=100; //buffer time
+//                    }
+//
+//                    List<HashMap<String, Object>> cheeseCounts = (List<HashMap<String, Object>>)wrapper.get("cheeseCountList");
+//                    List<ParseObject> allCountList = new ArrayList<ParseObject>();
+//                    for(HashMap<String, Object> eachCount : cheeseCounts){
+//
+//                        String friendFacebookId = (String)eachCount.get("facebookId");
+//                        int cheeseCount = (Integer)eachCount.get("cheeseCount");
+//                        boolean showMe = (Boolean)eachCount.get("showMe");
+//                        boolean animateMe = (Boolean)eachCount.get("animateMe");
+//
+//                        ParseObject tempObject = new ParseObject("cheeseCountObj");
+//                        tempObject.put("facebookId", friendFacebookId);
+//                        tempObject.put("cheeseCount", cheeseCount);
+//                        tempObject.put("showMe", showMe);
+//                        tempObject.put("animateMe", animateMe);
+//
+//                        allCountList.add(tempObject);
+//                    }
+//
+//                    ParseObject.pinAllInBackground(StealTheCheeseApplication.PIN_TAG, allCountList, new SaveCallback() {
+//                        @Override
+//                        public void done(ParseException ex) {
+//                            startTheftActivity();
+//
+//                        }
+//                    });
+//                }
+//            }
+//        });
+    }
+
+
+
+    /**
+     * TODO : FIX ME !!!
+     */
+    private void startTheftActivity() {
+//        Intent intent = new Intent(LoginActivity.this, TheftActivity.class);
+//        // removing this activity from backstack
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        String updateType = getResources().getString(R.string.update_type);
+//        intent.putExtra(updateType, UpdateType.LOGIN);
+//        intent.putExtra("CountDown", timeLeft);
+//        startActivity(intent);
+//        finish();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+//        ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
+    }
+
 }
